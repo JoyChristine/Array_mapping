01b7f104990b062cb1642df9db638e0bcd41672b
Joy Christine <joychristin2@gmail.com>
2022-03-07 14:41:37 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//looping using forEach
+// var languages = ["html","css","JS"];
+// languages.forEach((language) => {
+//     console.log("I like" + " " + language)
+// })
+
+//double through an array
+// var numbers = [1,2,3,4,5,6];
+// var doubledNumbers = [];
+// numbers.forEach((number) => {
+//     doubledNumbers.push(number * 2);
+// });
+// console.log(doubledNumbers);
+
+//using map() to double
+// var numbers =[1,2,3,4,5,6];
+// var doubledNumbers = numbers.map((number) => {
+//     return number * 2;
+// });
+// console.log(doubledNumbers);
+
+//code to loop through a deck of cards
+// need 2 loops, suit & values i.e., spades, 2
+// var deck =[];
+// var suits =["diamonds","spades","hearts","clubs"];
+// var faces =["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+// suits.forEach((suit) => {
+//     faces.forEach((face) => {
+//         deck.push(`${face} of ${suit}`);
+//     });
+// });
+// console.log(deck)
+
